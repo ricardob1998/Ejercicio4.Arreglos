a2c2236ce02aa3f7b6beb7eca23ed5c99632a212
ricardob1998 <86386658+ricardob1998@users.noreply.github.com>
2021-06-24 11:44:58 -0600

    Primer commit

---
+++ Ejercicio4.Arreglos/FormEjercicio4.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace Ejercicio4.Arreglos
+{
+    partial class FormEjercicio4
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NombretextBox = new System.Windows.Forms.TextBox();
+            this.EdadtextBox = new System.Windows.Forms.TextBox();
+            this.MuestracomboBox = new System.Windows.Forms.ComboBox();
+            this.ingresarbutton = new System.Windows.Forms.Button();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(29, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(114, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nombre del Estudiante";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(29, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Edad del Estudiante";
+            // 
+            // NombretextBox
+            // 
+            this.NombretextBox.Location = new System.Drawing.Point(140, 21);
+            this.NombretextBox.Name = "NombretextBox";
+            this.NombretextBox.Size = new System.Drawing.Size(232, 20);
+            this.NombretextBox.TabIndex = 2;
+            // 
+            // EdadtextBox
+            // 
+            this.EdadtextBox.Location = new System.Drawing.Point(140, 54);
+            this.EdadtextBox.Name = "EdadtextBox";
+            this.EdadtextBox.Size = new System.Drawing.Size(48, 20);
+            this.EdadtextBox.TabIndex = 3;
+            // 
+            // MuestracomboBox
+            // 
+            this.MuestracomboBox.FormattingEnabled = true;
+            this.MuestracomboBox.Location = new System.Drawing.Point(111, 103);
+            this.MuestracomboBox.Name = "MuestracomboBox";
+            this.MuestracomboBox.Size = new System.Drawing.Size(121, 21);
+            this.MuestracomboBox.TabIndex = 4;
+            // 
+            // ingresarbutton
+            // 
+            this.ingresarbutton.Location = new System.Drawing.Point(297, 54);
+            this.ingresarbutton.Name = "ingresarbutton";
+            this.ingresarbutton.Size = new System.Drawing.Size(107, 43);
+            this.ingresarbutton.TabIndex = 5;
+            this.ingresarbutton.Text = "INGRESAR";
+            this.ingresarbutton.UseVisualStyleBackColor = true;
+            this.ingresarbutton.Click += new System.EventHandler(this.ingresarbutton_Click);
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(297, 103);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(107, 49);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "GUARDAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(36, 172);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(107, 49);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Forma Ascendente";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(165, 172);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(107, 49);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Forma Descendente";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // FormEjercicio4
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 350);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.ingresarbutton);
+            this.Controls.Add(this.MuestracomboBox);
+            this.Controls.Add(this.EdadtextBox);
+            this.Controls.Add(this.NombretextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "FormEjercicio4";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NombretextBox;
+        private System.Windows.Forms.TextBox EdadtextBox;
+        private System.Windows.Forms.ComboBox MuestracomboBox;
+        private System.Windows.Forms.Button ingresarbutton;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Ejercicio4.Arreglos/FormEjercicio4.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio4.Arreglos
+{
+    public partial class FormEjercicio4 : Form
+    {
+        public FormEjercicio4()
+        {
+            InitializeComponent();
+        }
+
+        ArrayList datos = new ArrayList();
+
+        private void ingresarbutton_Click(object sender, EventArgs e)
+        {
+            if (NombretextBox.Text == "")
+            {
+                errorProvider1.SetError(NombretextBox, "Debe Ingresar El Nombre Del Estudiante");
+                NombretextBox.Focus();
+                return;
+            }
+            else if (EdadtextBox.Text == "")
+            {
+                errorProvider1.SetError(EdadtextBox, "Debe Ingresar La Edad Del Estudiante");
+                EdadtextBox.Focus();
+                return;
+            }
+            else
+            {
+                errorProvider1.SetError(NombretextBox, "");
+                errorProvider1.SetError(EdadtextBox, "");
+            }
+
+            datos.Add(NombretextBox.Text + " | " + EdadtextBox.Text);
+            EdadtextBox.Text = "";
+            NombretextBox.Text = "";
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < datos.Count; i++)
+            {
+                MuestracomboBox.Items.Add(datos[i]);
+                
+            }
+            MessageBox.Show("Estudiante Guardado");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MuestracomboBox.Items.Clear();
+            for (int i = 0; i < datos.Count; i++)
+            {
+                datos.Sort();
+                MuestracomboBox.Items.Add(datos[i]);
+                
+            }
+            MessageBox.Show("Nombres De Estudiantes de Forma Ascendentes");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            MuestracomboBox.Items.Clear();
+            for (int i = 0; i < datos.Count; i++)
+            {
+                datos.Sort();
+                datos.Reverse();
+                MuestracomboBox.Items.Add(datos[i]);
+                
+            }
+            MessageBox.Show("Nombres De Estudiante de Forma Descendentes");
+        }
+    }
+}
